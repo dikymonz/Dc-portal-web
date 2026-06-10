77d1579d50daa108599b11712104cbac3c765501
M. Diki Iswari <141617567+dikymonz@users.noreply.github.com>
2026-06-10 23:50:48 +0800

    .

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import ServerStats from "../components/ServerStats";
+import Footer from "../components/Footer";
+import amelGif from "../assets/img/amel.gif";
+
+export default function Home() {
+  useEffect(() => {
+    let link = document.querySelector("link[rel='icon']");
+
+    if (!link) {
+      link = document.createElement("link");
+      link.rel = "icon";
+      document.head.appendChild(link);
+    }
+
+    link.href = amelGif;
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <ServerStats />
+      </main>
+      <Footer />
+    </>
+  );
+}
